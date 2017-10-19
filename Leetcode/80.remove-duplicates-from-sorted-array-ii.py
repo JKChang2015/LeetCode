786cebe5f40b044465ea7a6ac9ed7d58822e1252
JKChang <jkchang2015@gmail.com>
2017-10-19 09:30:26 +0100

    update 80.remove-duplicates-from-sorted-array-ii.py

--- Leetcode/80.remove-duplicates-from-sorted-array-ii.py
+++ Leetcode/80.remove-duplicates-from-sorted-array-ii.py
@@ -39,3 +39,14 @@ class Solution(object):
                 index += 1
         return index
             
+#     def removeDuplicates(self, A):
+#         if len(A) <= 2: return len(A)
+#         prev = 1; curr = 2
+#         while curr < len(A):
+#             if A[curr] == A[prev] and  A[curr] == A[prev - 1]:
+#                 curr += 1
+#             else:
+#                 prev += 1
+#                 A[prev] = A[curr]
+#                 curr += 1
+#         return prev + 1
